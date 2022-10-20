3ed6bcf73bf235f8974f64e52fb6b754ed2f04a6
tenisaname <andreeivanovich@gmail.com>
2022-10-20 20:52:35 +0300

    Добавление управления кораблем

--- alien_invasion.py
+++ alien_invasion.py
@@ -14,14 +14,15 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height)) #Доступ к дисплею
         pygame.display.set_caption("Alien Invasion") #Добавление названия игры в верхней части экрана заголовка.
         self.ship = Ship(self)
-        #Назначение цвета фона.
-        self.bg_color = (230,230,230)
+
+        self.bg_color = Settings().bg_color
 
     def run_game(self):
         """Запуск основного цикла игры"""
         while True:
             self._check_events()
             self._update_screen()
+            self.ship.update()
             #Отслеживание событий клавиатуры и мыши
 
     def _check_events(self):
@@ -30,6 +31,18 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+                    #Переместить корабль вправо
+                    self.ship.moving_right = True
+                if event.key == pygame.K_LEFT or event.key == pygame.K_a:
+                    self.ship.moving_left = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+                    self.ship.moving_right = False
+                if event.key == pygame.K_LEFT or event.key == pygame.K_a:
+                    self.ship.moving_left = False
+
 
     def _update_screen(self):
         """Обновляет изображение на экран и отображает новый экран"""
--- car.py
+++ car.py
@@ -1,6 +1,6 @@
 import pygame
 
-class Ship():
+class Car():
     """Класс для управления кораблем"""
 
     def __init__(self,ai_game):
@@ -11,22 +11,12 @@ class Ship():
         self.screen_rect = ai_game.screen.get_rect()
 
         #Загружает изобржание корабля и получает прямоугольник
-        self.image = pygame.image.load("images/ship.bmp")
+        self.image = pygame.image.load("images/car.bmp")
         #Позиционирование корабля
         self.rect = self.image.get_rect()
         #Каждый новый корабль появляется у нижнего края экрана.
-        self.rect.midbottom = self.screen_rect.midbottom
+        self.rect.center = self.screen_rect.center
 
-        #Флаг перемещения
-        self.moving_right = False
-        self.moving_left = False
-
-    def update(self):
-        """Обновляет позицию корабля с учетом флага"""
-        if self.moving_right:
-            self.rect.x += 1
-        if self.moving_left:
-            self.rect.x -= 1
     def blitme(self):
         """Рисует корабль в текущей позиции"""
         self.screen.blit(self.image, self.rect)
